a14395073fdea11acf70bb632ff9d85e942cfd3d
Dinesh <dinesh2952002@outlook.com>
2023-11-29 12:17:21 +0530

    updated the smart contract

---
+++ contracts/audio.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Audio {
+    address public owner;
+    mapping(address => bool) public hasAccess;
+    mapping(address => bool) public hasListened;
+    mapping(uint256 => string) public audioUrls; // Store URLs for each audio
+
+    event PaymentReceived(address indexed payer, uint256 amount);
+    event AudioPlayed(address indexed listener);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    modifier requirePayment() {
+        require(
+            msg.value >= 0.0001 ether,
+            "Insufficient payment. Please pay 0.0001 ETH to access the audio."
+        );
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+
+        // Set predefined audio URLs during contract deployment
+        audioUrls[
+            1
+        ] = "https://gateway.pinata.cloud/ipfs/QmYd6QqjAsf1BPGijq4kAmuVXPBbvDRWqyprRdAPzLcXpa";
+        audioUrls[
+            2
+        ] = "https://gateway.pinata.cloud/ipfs/QmexfW4Vu6qvgudCSjDjPqTAv1ZYjLDrkDsYBeUtdNxkhE";
+        audioUrls[
+            3
+        ] = "https://gateway.pinata.cloud/ipfs/QmTApCdTVWddkLkJ8PCHADNjDUvcpj1hMhHzTULs3V1CTe";
+        audioUrls[
+            4
+        ] = "https://gateway.pinata.cloud/ipfs/QmTskujoRo3wiqN1BGsB111aRebdENNgNjJrWwAK6W6FPL";
+    }
+
+    receive() external payable {
+        emit PaymentReceived(msg.sender, msg.value);
+        hasAccess[msg.sender] = true;
+        hasListened[msg.sender] = false; // Initialize the listened state to false
+    }
+
+    // Function to get the URL of a specific audio
+    function getAudioUrl(
+        uint256 audioId
+    ) external view returns (string memory) {
+        return audioUrls[audioId];
+    }
+
+    function listenToAudio(uint256 audioId) external payable requirePayment {
+        require(hasAccess[msg.sender], "You need to pay to access the audio.");
+        require(
+            !hasListened[msg.sender],
+            "You have already listened to the audio."
+        );
+
+        // Logic to play the audio goes here
+        //string memory url = audioUrls[audioId];
+
+        // Additional logic to handle the audio URL (e.g., emit an event)
+        emit AudioPlayed(msg.sender);
+
+        hasListened[msg.sender] = true;
+    }
+
+    function grantAccess(address user) external onlyOwner {
+        hasAccess[user] = true;
+    }
+
+    function revokeAccess(address user) external onlyOwner {
+        hasAccess[user] = false;
+    }
+}
